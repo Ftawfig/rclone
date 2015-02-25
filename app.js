2881e3a1be792282d60357f9dd02825d384d05ab
ftawfig <ftawfig@gmail.com>
2015-02-25 03:23:23 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+//RClone
+//Fadi Tawfig
+//LM: 02/25/2015  
+
+var app = angular.module('RClone', []); 
+
+app.controller('MainCtrl', [
+    '$scope',
+    function($scope)
+    {
+        $scope.test = 'Hello World!';
+        
+        $scope.posts = [
+            {title: 'post 1', points: 52},
+            {title: 'post 2', points: 4},
+            {title: 'post 3', points: 1977},
+            {title: 'post 4', points: 42},
+            {title: 'post 5', points: 47}
+        ];
+        
+        $scope.addPost = function()
+        {
+            if(!$scope.postTitle || $scope.postTitle === '') 
+            {
+                return;
+            }
+            
+            //validate $scope.postLink
+            if( $scope.postLink.substr(0, 7) !== "http://" ) 
+            {
+                //Add http:// prefix if not provided
+                $scope.postLink = "http://" + $scope.postLink;
+                //more validation later, this is enough to get links to work
+            }
+            
+            $scope.posts.push({ title: $scope.postTitle, points: 0, link: $scope.postLink });
+            $scope.postTitle = '';
+            $scope.postLink = '';
+            
+            return;
+        };
+        
+        $scope.incrementPoints = function(post)
+        {
+            post.points++;
+            return;
+        };
+        
+        $scope.decrementPoints = function(post)
+        {
+            post.points--;
+            return;
+        };
+        
+        //UI not yet implemented
+        $scope.deletePost = function(post)
+        {
+            $scope.posts.pop(post);
+        };
+    }
+    
+]);
