545c3c38fa729d442d2ee6ea9a13f82745ebcf82
ftawfig <ftawfig@gmail.com>
2015-02-26 18:48:55 -0500

    Post logic now in service

--- app.js
+++ app.js
@@ -1,39 +1,77 @@
 //RClone
 //Fadi Tawfig
-//LM: 02/25/2015  
+//LM: 02/26/2015  
 
 var app = angular.module('RClone', []); 
 
-app.controller('MainCtrl', [
-    '$scope',
-    function($scope)
+app.factory('posts', 
+    [function() 
     {
-        $scope.test = 'Hello World!';
+        var o = {
+            posts: []
+        };
         
-        $scope.posts = [
+        //Test posts
+        o.posts = [
             {title: 'Youtube', points: 52, link: "http://www.youtube.com"},
-            {title: 'post 2', points: 4},
+            {title: 'Reddit', points: 4, link: "http://www.reddit.com"},
             {title: 'Google', points: 1977, link: "http://google.com"},
             {title: 'post 4', points: 42},
             {title: 'post 5', points: 47}
         ];
         
-        $scope.addPost = function()
+        o.addPost = function(postTitle, postLink)
         {
-            if(!$scope.postTitle || $scope.postTitle === '') 
+            if(!postTitle || postTitle === '') 
             {
                 return;
             }
             
             //validate $scope.postLink
-            if( $scope.postLink.substr(0, 7) !== "http://" ) 
+            if(postLink && postLink.substr(0, 7) !== "http://") 
             {
                 //Add http:// prefix if not provided
-                $scope.postLink = "http://" + $scope.postLink;
+                postLink = "http://" + postLink;
                 //more validation later, this is enough to get links to work
             }
             
-            $scope.posts.push({ title: $scope.postTitle, points: 0, link: $scope.postLink });
+            this.posts.push({ title: postTitle, points: 0, link: postLink});
+
+            return;
+        };
+        
+        o.incrementPoints = function(post)
+        {
+            post.points++;
+            return;
+        };
+        
+        o.decrementPoints = function(post)
+        {
+            post.points--;
+            return;
+        };
+        
+        //UI not yet implemented
+        o.deletePost = function(post)
+        {
+            posts.posts.pop(post);
+        };
+        
+        return o;
+    }]);
+
+app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function($scope, posts)
+    {
+        $scope.posts = posts.posts;
+        
+        $scope.addPost = function()
+        {
+            posts.addPost($scope.postTitle, $scope.postLink);
+                        
             $scope.postTitle = '';
             $scope.postLink = '';
             
@@ -42,20 +80,21 @@ app.controller('MainCtrl', [
         
         $scope.incrementPoints = function(post)
         {
-            post.points++;
+            posts.incrementPoints(post);
             return;
         };
         
         $scope.decrementPoints = function(post)
         {
-            post.points--;
+            posts.decrementPoints(post);
             return;
         };
         
         //UI not yet implemented
         $scope.deletePost = function(post)
         {
-            $scope.posts.pop(post);
+            posts.deletePost(post);
+            return;
         };
     }
     
