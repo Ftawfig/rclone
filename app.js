e05645dc4648d4bddf1059ede30f3b67392d0f74
ftawfig <ftawfig@gmail.com>
2015-03-01 00:27:08 -0500

    Post view implemented, comments can be viewed, added, and voted on.

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 //RClone
 //Fadi Tawfig
-//LM: 02/26/2015  
+//LM: 02/28/2015  
 
-var app = angular.module('RClone', []); 
+var app = angular.module('RClone', ['ui.router']); 
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) 
+    {
+        $stateProvider.state('home', { url: '/home', templateUrl: 'home.html', controller: 'MainCtrl'});
+        
+        $stateProvider.state('nav', { url: '/nav', templateUrl: 'navbar.html'});
+        
+        $stateProvider.state('posts', { url: '/post/{id}', templateUrl: 'post.html', controller: 'PostCtrl'});
+        
+        $urlRouterProvider.otherwise('home');
+    }]);
 
 app.factory('posts', 
     [function() 
@@ -13,11 +27,11 @@ app.factory('posts',
         
         //Test posts
         o.posts = [
-            {title: 'Youtube', points: 52, link: "http://www.youtube.com"},
-            {title: 'Reddit', points: 4, link: "http://www.reddit.com"},
-            {title: 'Google', points: 1977, link: "http://google.com"},
-            {title: 'post 4', points: 42},
-            {title: 'post 5', points: 47}
+            {title: 'Youtube', points: 52, link: "http://www.youtube.com", comments: []},
+            {title: 'Reddit', points: 4, link: "http://www.reddit.com", comments: []},
+            {title: 'Google', points: 1977, link: "http://google.com", comments: []},
+            {title: 'post 4', points: 42, comments: []},
+            {title: 'post 5', points: 47, comments: []}
         ];
         
         o.addPost = function(postTitle, postLink)
@@ -35,7 +49,15 @@ app.factory('posts',
                 //more validation later, this is enough to get links to work
             }
             
-            this.posts.push({ title: postTitle, points: 0, link: postLink});
+            //Add post to array with test comments
+            
+            this.posts.push({ title: postTitle, points: 0, link: postLink, 
+                                comments: 
+                                [
+                                    {author: "Fadi", text: "Real shit", points: 0},
+                                    {author: "Long", text: "This is me typing a sentance. I am typing another sentance. I am trying to see what a longer comment will look like. Swear words, swear words, swear words.", points: 11}
+                                ]
+                            });
 
             return;
         };
@@ -56,6 +78,20 @@ app.factory('posts',
         o.deletePost = function(post)
         {
             posts.posts.pop(post);
+            return;
+        };
+        
+        o.addComment = function(userName, commentText, postId)
+        {
+            //userName and commentText may not be empty
+            if(!userName || userName === '' || !commentText || commentText === '')
+            {
+                return;
+            }
+            
+            this.posts[postId].comments.push({author: userName, text: commentText, points: 0});
+            
+            return;
         };
         
         return o;
@@ -99,3 +135,35 @@ app.controller('MainCtrl', [
     }
     
 ]);
+
+app.controller('PostCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts) 
+    {
+        $scope.post = posts.posts[$stateParams.id];
+        
+        $scope.incrementPoints = function(comment) 
+        {
+            posts.incrementPoints(comment);
+            return;
+        };
+        
+        $scope.decrementPoints = function(comment)
+        {
+            posts.decrementPoints(comment);
+            return;
+        };
+        
+        $scope.addComment = function()
+        {
+            posts.addComment($scope.userName, $scope.commentText, $stateParams.id);
+            
+//            $scope.userName = '';
+//            $scope.commentText = '';
+            
+            return;
+        };
+        
+    }]);
